69df29e20b12a81d5fe9c2394bc4e1cbb15f3aa7
EsGeh <SamuelGfroerer@googlemail.com>
2013-10-17 10:55:13 +0200

    slight generalisation

--- SGMath/GCD.hs
+++ SGMath/GCD.hs
@@ -3,8 +3,7 @@ module SGMath.GCD where
 import Data.Monoid
 import Data.Ratio
 
-
-gcd' :: Rational -> Rational-> Rational
+gcd' :: Integral i => Ratio i -> Ratio i -> Ratio i
 gcd' x y = (recip (commonDivisor%1)) * ( (gcd nomX' nomY') % 1)
 	where
 		nomX = numerator x ; denomX = denominator x
@@ -12,10 +11,13 @@ gcd' x y = (recip (commonDivisor%1)) * ( (gcd nomX' nomY') % 1)
 		nomX' = numerator ( x * ((commonDivisor % denomX)))
 		nomY' = numerator ( y * ((commonDivisor % denomY)))
 		commonDivisor = lcm denomX denomY
+
 lcm' x y = x * y / gcd' x y
 
+{-
 newtype GCD = GCD { getGCD :: Rational}
 	deriving(Show)
 instance Monoid GCD where
 	mempty = GCD 1
 	(GCD x) `mappend` (GCD y) = GCD $ gcd' x y
+-}
